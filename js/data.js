32c62a807e431d5897983a421cdbb21d2b7bbfea
Leo15th <pwaiag2023@gmail.com>
2026-04-24 18:47:42 +0630

    update on 24/4/2026

---
+++ js/data.js
@@ -0,0 +1,93 @@
+const menuItems = [
+    {
+        id: 1,
+        name: "Strawberry Shake",
+        category: "Shake",
+        price: 2.5,
+        img: "assets/menuItems/strawberryShake.jpg"
+    },
+    {
+        id: 2,
+        name: "Blueberry Shake",
+        category: "Shake",
+        price: 3.5,
+        img: "assets/menuItems/blueberryShake.jpg"
+    },
+    {
+        id: 3,
+        name: "Pineapple Shake",
+        category: "Shake",
+        price: 2.9,
+        img: "assets/menuItems/pineappleShake.jpg"
+    },
+    {
+        id: 4,
+        name: "Apple Shake",
+        category: "Shake",
+        price: 5.4,
+        img: "assets/menuItems/appleShake.jpg"
+    },
+    {
+        id: 5,
+        name: "Avocado Shake",
+        category: "Shake",
+        price: 5.5,
+        img: "assets/menuItems/avocadoShake.jpg"
+    },
+    {
+        id: 6,
+        name: "Orange Shake",
+        category: "Shake",
+        price: 3.5,
+        img: "assets/menuItems/orangeShake.jpg"
+    },
+    {
+        id: 7,
+        name: "Grape Shake",
+        category: "Shake",
+        price: 2.9,
+        img: "assets/menuItems/grapeShake.jpg"
+    },
+    {
+        id: 8,
+        name: "Coconut Shake",
+        category: "Shake",
+        price: 5.4,
+        img: "assets/menuItems/coconutShake.jpg"
+    },
+    {
+        id: 9,
+        name: "Avocado Smoothie",
+        category: "Smoothie",
+        price: 4.3,
+        img: "assets/menuItems/avocadoSmoothie.jpg"
+    },
+    {
+        id: 10,
+        name: "Latte",
+        category: "Coffee",
+        price: 4.3,
+        img: "assets/menuItems/coffeeLatte.jpg"
+    },
+    {
+        id: 11,
+        name: "Yellow Tea",
+        category: "Tea",
+        price: 4.3,
+        img: "assets/menuItems/yellowTea.jpg"
+    },
+    {
+        id: 12,
+        name: "Blue Huawaii",
+        category: "Mocktail",
+        price: 4.3,
+        img: "assets/menuItems/blueHawaii.jpg"
+    },
+    {
+        id: 13,
+        name: "Bubble Tea",
+        category: "Boba",
+        price: 4.3,
+        img: "assets/menuItems/bubbleTea.jpg"
+    },
+]
--- js/userPage.js
+++ js/userPage.js
@@ -1,12 +1,9 @@
 //reference nav
-const nav = document.querySelector("nav")
 const userNameSpan = document.getElementById("userNameSpan")
 
 //reference cart
 const cartIcon = document.querySelector("#cartContainer .fa-cart-shopping")
-
 const cartCountContainer = document.getElementById("cartCountContainer")
-
 const cartCountSpan = document.getElementById("cartCount");
 
 //reference test btn
@@ -17,53 +14,150 @@ const rfcBtn = document.getElementById("rfcBtn")
 const tabBtns = document.querySelectorAll("#categoryBtnsContainer .tabBtn")
 const tabContents = document.querySelectorAll("#tabContentHolder .tabContent")
 
+let userKey;
 
 //window event for userName
 window.addEventListener("DOMContentLoaded", ()=>{
     const currentUser = JSON.parse(localStorage.getItem("currentUser"))
     if(currentUser){
-        userNameSpan.textContent = currentUser.userName
+        userNameSpan.textContent = currentUser.userName;
+        userKey = `cart_${currentUser.userName}`
+        updateCartCount(userKey)
     }
-})
 
-//atc events
-let cartCountNumber = Number(cartCountSpan.innerText);
-
-atcBtn.addEventListener("click", ()=>{
-    cartCountNumber ++;
-    cartCountSpan.innerText = Number(cartCountNumber);
-    if (cartCountNumber >0){
-        cartCountContainer.style.display = "flex"
+    const activeTab = document.querySelector(".tabBtn.active")
+    if(activeTab) {
+        const category = activeTab.dataset.category;
+        renderMenu(category, userKey)
     }
-    console.log(cartCountNumber)
-    console.log(typeof(cartCountNumber))
-})
-rfcBtn.addEventListener("click",()=>{
-    if(cartCountNumber>0){
-        cartCountNumber--;
-    }
-    if(cartCountNumber === 0){
-        cartCountContainer.style.display = "none";
-    }else {
-        cartCountSpan.innerText = cartCountNumber;
-    }
-    console.log(cartCountNumber)
 })
 
 //window scroll event
-//event listener for scroll event
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 30) {
-    nav.style.borderBottom = "1px solid rgba(255,255,255, 0.5)";
-    nav.style.background = "rgba(166, 166, 166, 0.6)";
-  } else {
-    nav.style.borderBottom = "3px solid rgba(170,170,170, 0.5)";
-    nav.style.background = "red"
-  }
-});
+
+
+
+// atc events
+let cartCountNumber = Number(cartCountSpan.innerText) || 0;
+if(cartCountNumber === 0){
+    cartCountContainer.style.display = "none"
+}
+
+//function for update Cart Count
+function updateCartCount (userKey){
+    let cart = JSON.parse(localStorage.getItem(userKey)) || [];
+    let cartCountNumber = cart.length;
+    cartCountSpan.innerText = cartCountNumber
+    if(cartCountNumber>0){
+        cartCountContainer.style.display= "flex"
+    } else{
+        cartCountContainer.style.display = "none"
+    }
+}
+
+//function for menu render
+function renderMenu(category, userKey){
+    const container = document.getElementById(category);
+    container.innerHTML = "";
+    
+    let cart = JSON.parse(localStorage.getItem(userKey)) || [];
+    
+    //get the image
+    const filteredCategory = menuItems.filter(menuItem => menuItem.category === category)
+
+    filteredCategory.forEach(menuItem =>{
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <img src="${menuItem.img}" alt="">
+            <div class="cardDescriptionHolder">
+                <div class="cardDescriptionHeader">
+                    <h3>${menuItem.name}</h3>
+                    <span>Price: $${menuItem.price}</span>
+                </div>
+                <button class="atcBtn">Add To Cart</button>
+                <div class="quantityControl">
+                    <button class="reduceQuantityBtn">-</button>
+                    <span class="quantityCount">0</span>
+                    <button class="increaseQuantityBtn">+</button>
+                </div>
+            </div>
+        `;
+        container.appendChild(card);
+        const atcBtn = card.querySelector(".atcBtn")
+
+        const quantityControlContainer = card.querySelector(".quantityControl")
+        const reduceQuantityBtn = card.querySelector(".reduceQuantityBtn") 
+        const quantityCount = card.querySelector(".quantityCount")
+        const increaseQuantityBtn = card.querySelector(".increaseQuantityBtn")
+        
+        const menuItemName = menuItem.name;
+        const menuItemPrice= menuItem.price;
+        const menuItemPhoto = menuItem.img
+        
+        let existingItem = cart.find(item => item.menuItemName === menuItemName);
+        let quantityNumber = existingItem ? existingItem.quantityNumber : 0 ;
+
+        if(quantityNumber> 0){
+            atcBtn.style.display = "none";
+            quantityControlContainer.style.display = "grid";
+            quantityCount.innerText = quantityNumber;
+        }
+        atcBtn.addEventListener("click", ()=> {
+            atcBtn.style.display = "none"
+            quantityNumber ++;
+            quantityCount.innerText = quantityNumber
+            quantityControlContainer.style.display = "grid"
+            //if have existing item
+            let existingItem = cart.find(item => item.menuItemName === menuItemName)
+            if(existingItem){
+                existingItem.quantityNumber = quantityNumber;
+            } else{
+                cart.push({
+                    menuItemName,
+                    menuItemPhoto,
+                    menuItemPrice,
+                    quantityNumber
+                })
+            }
+            localStorage.setItem(userKey, JSON.stringify(cart));
+            updateCartCount(userKey);
+        })
+        reduceQuantityBtn.addEventListener("click",()=>{
+            if(quantityNumber > 0){
+                quantityNumber--;
+                quantityCount.innerText = quantityNumber;
+            }
+            if(quantityNumber === 0){
+                quantityControlContainer.style.display = "none"
+                atcBtn.style.display = "block";
+                cart = cart.filter(item => item.menuItemName !== menuItemName)
+            } else{
+                let existingItem = cart.find(item => item.menuItemName === menuItemName);
+                if (existingItem) {
+                existingItem.quantityNumber = quantityNumber;
+            }
+            }
+            localStorage.setItem(userKey, JSON.stringify(cart));
+            updateCartCount(userKey)
+        })
+
+        increaseQuantityBtn.addEventListener("click",()=>{
+            quantityNumber++;
+            quantityCount.innerText = quantityNumber
+            let existingItem = cart.find(item=> item.menuItemName === menuItemName);
+            if (existingItem){
+                existingItem.quantityNumber = quantityNumber;
+            }
+            localStorage.setItem(userKey, JSON.stringify(cart))
+            updateCartCount(userKey)
+        })
+    })
+
+}
 
 //function for the tab btns for tabcategory
 function opentab(event, tabName){
+    console.log("Switching to tab:", tabName);
     tabContents.forEach(tc => {
         tc.classList.remove("active")
     })
@@ -72,4 +166,5 @@ function opentab(event, tabName){
     })
     document.getElementById(tabName).classList.add("active");
     event.currentTarget.classList.add("active")
+    renderMenu(tabName, userKey);
 }
